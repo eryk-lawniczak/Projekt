ff3ec0f8b88d25915c8298dadd7cbf9e729ebfa7
eryk-lawniczak <eryk.lawniczak@gmail.com>
2018-11-28 18:50:04 +0100

    Revert "Update informacje.php"
    This reverts commit 7dbaf36b47f02db46c6176f7bc858302e87f8635.

--- informacje.php
+++ informacje.php
@@ -12,38 +12,10 @@
           </div>
           <div class="w-100"></div>
           <div class="col-6">
-            <table class="text-center">
+            <table>
               <tr>
                 <th>Dzień</th>
                 <th>Godzina</th>
-              </tr>
-              <tr>
-                <td>Poniedziałel</td>
-                <td>9-17</td>
-              </tr>
-              <tr>
-                <td>Wtorek</td>
-                <td>7-17</td>
-              </tr>
-              <tr>
-                <td>Środa</td>
-                <td>7-17</td>
-              </tr>
-              <tr>
-                <td>Czwartek</td>
-                <td>7-16</td>
-              </tr>
-              <tr>
-                <td>Piątek</td>
-                <td>7-14</td>
-              </tr>
-              <tr>
-                <td>Sobota</td>
-                <td>10-14</td>
-              </tr>
-              <tr>
-                <td>Niedziela</td>
-                <td>Nieczynne</td>
               </tr>
             </table>
           </div>
